aae2d61d1a67fff5808e0ea9e92c38eb30369082
SuguruOoki <oki.work0527@gmail.com>
2022-07-22 18:02:44 +0900

    [MODIFY]https://actix.rs/docs/application/, state

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,65 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer};
 
 // #[get("/{id}/{name}/index.html")]
 // async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
 //     format!("Hello {}! id:{}", name, id)
 // }
 
-async fn index() -> impl Responder {
-    "Hello world!aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // HttpServer::new(|| App::new().service(index))
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .service(
-                // prefixes all resources and routes attached to it...
-                web::scope("/app")
-                    // ...so this handles requests for `GET /app/index.html`
-                    .route("/index.html", web::get().to(index)),
-            )
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
     })
-    .bind("127.0.0.1:8080")?
+    // HttpServer::new(|| {
+    // App::new()
+    //     .app_data(web::Data::new(AppState {
+    //         app_name: String::from("Actix Web"),
+    //     }))
+    //     .service(index)
+    // .service(
+    //     // prefixes all resources and routes attached to it...
+    //     web::scope("/app")
+    //         // ...so this handles requests for `GET /app/index.html`
+    //         .route("/index.html", web::get().to(index)),
+    // )
+    // .service(hello)
+    // })
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
+    // HttpServer::new(|| App::new().service(index))
+    // HttpServer::new(|| {
+    //     App::new()
+    //         // .service(hello)
+    //         // .service(echo)
+    //         .service(
+    //             // prefixes all resources and routes attached to it...
+    //             web::scope("/app")
+    //                 // ...so this handles requests for `GET /app/index.html`
+    //                 .route("/index.html", web::get().to(index)),
+    //         )
+    //     // .route("/hey", web::get().to(manual_hello))
+    // })
+    // .bind("127.0.0.1:8080")?
+    // .run()
+    // .await
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!2222")
+// This struct represents state
+struct AppState {
+    app_name: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!aaa")
-}
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!aaa")
+// }
