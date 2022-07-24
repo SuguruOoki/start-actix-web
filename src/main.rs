dfae4bef674d220c6c5e02372a2b991dce8b57c7
SuguruOoki <oki.work0527@gmail.com>
2022-07-24 13:04:09 +0900

    [REMOVE]必要のないコメントアウトを削除

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-// #[get("/{id}/{name}/index.html")]
-// async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-//     format!("Hello {}! id:{}", name, id)
-// }
-
 #[get("/")]
 async fn index(data: web::Data<AppState>) -> String {
     let app_name = &data.app_name; // <- get app_name
@@ -20,49 +15,15 @@ async fn main() -> std::io::Result<()> {
             }))
             .service(index)
     })
-    // HttpServer::new(|| {
-    // App::new()
-    //     .app_data(web::Data::new(AppState {
-    //         app_name: String::from("Actix Web"),
-    //     }))
-    //     .service(index)
-    // .service(
-    //     // prefixes all resources and routes attached to it...
-    //     web::scope("/app")
-    //         // ...so this handles requests for `GET /app/index.html`
-    //         .route("/index.html", web::get().to(index)),
-    // )
-    // .service(hello)
-    // })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-    // HttpServer::new(|| App::new().service(index))
-    // HttpServer::new(|| {
-    //     App::new()
-    //         // .service(hello)
-    //         // .service(echo)
-    //         .service(
-    //             // prefixes all resources and routes attached to it...
-    //             web::scope("/app")
-    //                 // ...so this handles requests for `GET /app/index.html`
-    //                 .route("/index.html", web::get().to(index)),
-    //         )
-    //     // .route("/hey", web::get().to(manual_hello))
-    // })
-    // .bind("127.0.0.1:8080")?
-    // .run()
-    // .await
 }
 
 // This struct represents state
 struct AppState {
     app_name: String,
 }
-
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!aaa")
-// }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
