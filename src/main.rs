eda3a6fedac4da4e96f68dcba96e96d58704e549
SuguruOoki <oki.work0527@gmail.com>
2022-07-24 13:48:38 +0900

    [MODIFY]Application guards and virtual hosting

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
-use std::sync::Mutex;
-
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
-}
-
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
-
-    format!("Request number: {counter}") // <- response with count
-}
+use actix_web::{guard, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Note: web::Data created _outside_ HttpServer::new closure
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-
-    HttpServer::new(move || {
-        // move counter into the closure
+    HttpServer::new(|| {
         App::new()
-            .app_data(counter.clone()) // <- register the created data
-            .route("/", web::get().to(index))
+            .service(
+                web::scope("/")
+                    .guard(guard::Header("Host", "www.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+            )
+            .service(
+                web::scope("/")
+                    .guard(guard::Header("Host", "users.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
+            )
+            .route("/", web::to(HttpResponse::Ok))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
